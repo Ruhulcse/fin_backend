436d9bd12cb8fc8edb2ea87f9839d1dcc4d821cc
Belayet Hossain <2020belayethossain@gmail.com>
2024-07-27 22:07:14 +0600

    db update

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const { PostgresDialect } = require("@sequelize/postgres");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 require("dotenv").config();
 const db_url = process.env.DB_URI;
@@ -12,23 +13,36 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(db_url,
-  {logging: false });
+  sequelize = new Sequelize(db_url, {
+    logging: false,
+    dialect: PostgresDialect,
+    database: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(file => {
+  .forEach((file) => {
     // console.log(`Importing model: ${file}`);
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     // console.log(`Imported model: ${model.name}`);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     // console.log(`Associating model: ${modelName}`);
     db[modelName].associate(db);
